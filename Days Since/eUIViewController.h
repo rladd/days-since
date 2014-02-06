72e158e88a0964a20f06954f8b4b9927267426e8
Ross Ladd <ross@element63.com>
2014-02-06 09:08:22 -0800

    Added Class.

---
+++ Days Since/eUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  eUIViewController.h
+//  Days Since
+//
+//  Created by Ross on 2/6/2014.
+//  Copyright (c) 2014 Element63. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface eUIViewController : UIViewController
+
+@end
---
+++ Days Since/eUIViewController.m
@@ -0,0 +1,38 @@
+//
+//  eUIViewController.m
+//  Days Since
+//
+//  Created by Ross on 2/6/2014.
+//  Copyright (c) 2014 Element63. All rights reserved.
+//
+
+#import "eUIViewController.h"
+
+@interface eUIViewController ()
+
+@end
+
+@implementation eUIViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
