b04302c0f39c8e814c67674d7b013a53dd4fef4a
Ross Ladd <ross@element63.com>
2014-02-11 06:00:18 -0800

    Stop button fix.

--- Days Since/eMainViewController.m
+++ Days Since/eMainViewController.m
@@ -24,8 +24,6 @@
     
     bool start;
     
-    __weak IBOutlet UILabel *testLabel;
-    
     NSTimeInterval time;
     
 }
@@ -51,19 +49,13 @@
     NSInteger nMins = [components minute];
     NSInteger nSec = [components second];
     
-    NSString *strTimer = [NSString stringWithFormat:@"%02d:%02d:%02d:%02d:%02d:%02d", (long)nYears, (long)nMonths, (long)nDays, (long)nHours, (long)nMins, (long)nSec];
+    NSString *strTimer = [NSString stringWithFormat:@"%02zu:%02zu:%02zu:%02zu:%02zu:%02zu", (long)nYears, (long)nMonths, (long)nDays, (long)nHours, (long)nMins, (long)nSec];
     
     self.timerLabel.text = strTimer;
     
-    //NSString *strTimer = [NSString stringWithFormat:@"Date: %@", tempRegCode];
-    //self.timerLabel.text = [NSString stringWithFormat:@"Date: %@", (long)days];
-    //NSLog(@"Years: %ld, Months: %ld, Days: %ld", (long)years, (long)months, (long)days);
-    
-    ///////////////////////////////////////////////////////////////////////////
-    //self.timerLabel.text = @"Date: %i";
-    
-    [self performSelector:@selector(updateTimer) withObject:self afterDelay:0.1];
-    
+    if (start == true) {
+        [self performSelector:@selector(updateTimer) withObject:self afterDelay:0.1];
+    }
 }
 
 
@@ -81,11 +73,11 @@
     if (start == false) {
         start = true;
         time = [NSDate timeIntervalSinceReferenceDate];
-        [sender setTitle:@"Stop!" forState:UIControlStateNormal];
+        [sender setTitle:@"Stop" forState:UIControlStateNormal];
         [self updateTimer];
     } else {
         start = false;
-        [sender setTitle:@"Start Timer" forState:UIControlStateNormal];
+        [sender setTitle:@"Start" forState:UIControlStateNormal];
     }
 }
 
