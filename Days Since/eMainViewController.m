4aff3924272640a61ebfd4a160564e2184642025
Ross Ladd <ross@element63.com>
2014-02-11 05:08:55 -0800

    Updated Timer.

--- Days Since/eMainViewController.m
+++ Days Since/eMainViewController.m
@@ -12,6 +12,10 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 
+@property NSDate *startDate;
+
+@property NSTimeInterval *secondsPerDay;
+
  - (IBAction)startButton:(id)sender;
 
 @end
@@ -20,38 +24,71 @@
     
     bool start;
     
+    __weak IBOutlet UILabel *testLabel;
+    
     NSTimeInterval time;
     
 }
 
 - (void) updateTimer
 {
-    if (start == false) {
-        return;
-    }
+    NSString *strEventDate = @"2013-11-08 12:00:00";
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate *eventDate = [dateFormatter dateFromString: strEventDate];
+    NSDate *currentDate = [NSDate date];
+    NSCalendar *gregorian = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
     
-    NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
-    NSTimeInterval elapsedTime = currentTime - time;
+    // pass as many or as little units as you like here, separated by pipes
+    NSUInteger units = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
     
-    int minutes = (int)(elapsedTime / 60.0);
-    int seconds = (int)(elapsedTime - (minutes * 60.0));
+    NSDateComponents *components = [gregorian components:units fromDate:eventDate toDate:currentDate options:0];
     
-    self.timerLabel.text = [NSString stringWithFormat:@"%u:%02u", minutes, seconds];
+    NSInteger nYears = [components year];
+    NSInteger nMonths = [components month];
+    NSInteger nDays = [components day];
+    NSInteger nHours = [components hour];
+    NSInteger nMins = [components minute];
+    NSInteger nSec = [components second];
+    
+    NSString *strTimer = [NSString stringWithFormat:@"%02d:%02d:%02d:%02d:%02d:%02d", (long)nYears, (long)nMonths, (long)nDays, (long)nHours, (long)nMins, (long)nSec];
+    
+    self.timerLabel.text = strTimer;
+    
+    //NSString *strTimer = [NSString stringWithFormat:@"Date: %@", tempRegCode];
+    //self.timerLabel.text = [NSString stringWithFormat:@"Date: %@", (long)days];
+    //NSLog(@"Years: %ld, Months: %ld, Days: %ld", (long)years, (long)months, (long)days);
+    
+    ///////////////////////////////////////////////////////////////////////////
+    //self.timerLabel.text = @"Date: %i";
     
     [self performSelector:@selector(updateTimer) withObject:self afterDelay:0.1];
     
 }
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	
     // Do any additional setup after loading the view.
-    self.timerLabel.text = @"0:00";
+    self.timerLabel.text = @"00:00:00:00:00:00";
  
     start = false;
     
 }
+- (IBAction)startButton:(id)sender {
+    if (start == false) {
+        start = true;
+        time = [NSDate timeIntervalSinceReferenceDate];
+        [sender setTitle:@"Stop!" forState:UIControlStateNormal];
+        [self updateTimer];
+    } else {
+        start = false;
+        [sender setTitle:@"Start Timer" forState:UIControlStateNormal];
+    }
+}
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -68,21 +105,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)startButton:(id)sender {
-    
-    if (start == false) {
-        start = true;
-        time = [NSDate timeIntervalSinceReferenceDate];
-        [sender setTitle:@"Stop!" forState:UIControlStateNormal];
-        [self updateTimer];
-    } else {
-        start = false;
-        [sender setTitle:@"Start Timer" forState:UIControlStateNormal];
-        
-    }
-    
-    
-}
 
 
 
